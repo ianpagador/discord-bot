b1ae088e8aa1b0186cf65326fb838ba58a2e9a2c
ianpagador <122330069+ianpagador@users.noreply.github.com>
2024-01-26 13:07:30 -0800

    Updated main.py
    Utilization of Bot rather than Client, converted the majority of text commands to account for the change

--- main.py
+++ main.py
@@ -1,164 +1,250 @@
 import discord
-from discord.ext import commands
-import os
-from dotenv import load_dotenv
-import yt_dlp as youtube_dl
-import json
+import random
 import requests
+import os
+import json
+import time
 import pandas as pd
 
-intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+intents = discord.Intents.all()
+intents.message_content = True
+
+client = discord.Bot(command_prefix='!', intents=intents)
 
 load_dotenv()
 df = pd.read_csv('name_id.csv')
-
 insult_status = False
 
+ballList = [
+    "https://media.giphy.com/media/3orieYvhT5EVfSFyBa/giphy.gif",
+    "https://media.giphy.com/media/3orifhNhn840GpMMPm/giphy.gif",
+    "https://media.giphy.com/media/xT5LMPGVX5evt3MU1i/giphy.gif",
+    "https://media.giphy.com/media/3orifcCRUXkwSsCikg/giphy.gif",
+    "https://media.giphy.com/media/xT5LMqFOUOHv4kMm8U/giphy.gif"
+]
+
+
+def getJoke():
+    response = requests.get(
+        "https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist&type=twopart")
+    json_data = json.loads(response.text)
+    setup = json_data["setup"]
+    punchline = json_data["delivery"]
+    totalJoke = setup + "-" + punchline
+    return totalJoke
+
+
+def getQuote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    author = " -" + json_data[0]["a"]
+    quote = json_data[0]["q"]
+    answer = quote + author
+    return answer
+    return response
+
 def getInsult(name):
-  response = requests.get("https://insult.mattbas.org/api/insult.json?who=" + name)
-  json_data = json.loads(response.text)
-  insult = json_data["insult"]
-  return insult
+    response = requests.get("https://insult.mattbas.org/api/insult.json?who=" + name)
+    json_data = json.loads(response.text)
+    insult = json_data["insult"]
+    return insult
 
-youtube_dl.utils.bug_reports_message = lambda: ''
+def randomCat():
+    response = requests.get("https://api.thecatapi.com/v1/images/search")
+    json_data = json.loads(response.text)
+    image = json_data[0]["url"]
+    return image
 
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
+def findDigit(inputMessage):
+  digitList = []
+  for c in inputMessage:
+    c.isdigit()
+    if c.isdigit() == True:
+      digitList.append(c)
+      print(digitList)
+    elif c.isdigit() == False:
+      stringDigits = [str(i) for i in digitList]
+      digitString = "".join(stringDigits)
+      numRoll = int(digitString)
+      print(numRoll)
+      return numRoll
 
-ffmpeg_options = {
-    'options': '-vn'
-}
+def findStr(inputMessage):
+  inputList = [str(inputMessage)]
+  strList = []
+  for char in inputList:
+   for c in char:
+       strList.append(c)
+  print(strList)
+  strCondition = False
+  while not strCondition:
+    for c in inputMessage:
+      c.isdigit()
+      if c.isdigit() == True:
+        print(strList)
+        del strList[0]
+        strCondition = False
+      elif c.isdigit() == False:
+        print(strList)
+        stringAll = [str(i) for i in strList]
+        inputString = "".join(stringAll)
+        print(inputString)
+        return inputString
+        break
 
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+def encrypt(numRoll, inputString):
+    newStuff = ""
+    for c in inputString:
+      textEncrypt = ord(c) + numRoll
+      newStuff += chr(textEncrypt)
+    return newStuff
 
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-        self.data = data
-        self.title = data.get('title')
-        self.url = ""
+def decrypt(numRoll, inputString):
+    newerStuff = ""
+    for c in inputString:
+      textDecrypt = ord(c) - numRoll
+      newerStuff += chr(textDecrypt)
+    return newerStuff
 
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-        filename = data['title'] if stream else ytdl.prepare_filename(data)
-        return filename
-
-@bot.event
-async def on_ready():
-  print("We have logged in as {0.user}".format(bot))
-
-@bot.command(name='join', help='Tells the bot to join the voice channel')
-async def join(ctx):
-    if not ctx.message.author.voice:
-        await ctx.send("connect to vc dummy")
-        return
-    else:
-        channel = ctx.author.voice.channel
-    await channel.connect()
-
-@bot.command(name='leave', help='To make the bot leave the voice channel')
-async def leave(ctx):
-    voice_client = ctx.message.guild.voice_client
-    if voice_client.is_connected():
-        await voice_client.disconnect()
-
-@leave.error
-async def leave_error(ctx, error: discord.DiscordException):
-    if isinstance(error, commands.CommandError):
-        await ctx.send("i'm not in vc stupid")
-    else:
-        raise error
-
-@bot.command(name='play_song', help='To play song')
-async def play(ctx,url):
-    try:
-        server = ctx.message.guild
-        voice_channel = server.voice_client
-        async with ctx.typing():
-            filename = await YTDLSource.from_url(url, loop=bot.loop)
-            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
-        await ctx.send('**now playing:** {}'.format(filename))
-    except:
-        await ctx.send("uh oh smth's wrong")
-
-@bot.command(name='pause', help='This command pauses the song')
-async def pause(ctx):
-    voice_client = ctx.message.guild.voice_client
-    if voice_client.is_playing():
-        await voice_client.pause()
-
-@pause.error
-async def pause_error(ctx, error: discord.DiscordException):
-    if isinstance(error, commands.CommandError):
-        await ctx.send("im not playing anything goofy")
-    else:
-        raise error
-
-@bot.command(name='resume', help='Resumes the song')
-async def resume(ctx):
-    voice_client = ctx.message.guild.voice_client
-    if voice_client.is_paused():
-        await voice_client.resume()
-
-@resume.error
-async def resume_error(ctx, error: discord.DiscordException):
-    if isinstance(error, commands.CommandError):
-        await ctx.send("what do you want me to resume lol")
-    else:
-        raise error
-
-@bot.command(name='stop', help='Stops the song')
-async def stop(ctx):
-    voice_client = ctx.message.guild.voice_client
-    if voice_client.is_playing():
-        await voice_client.stop()
-
-@stop.error
-async def stop_error(ctx, error: discord.DiscordException):
-    if isinstance(error, commands.CommandError):
-        await ctx.send("nothing was playing are you delulu")
-    else:
-        raise error
-
-@bot.command(name='insults', help='Turn insults on or off')
+@client.command(name='insults', help='Turn insults on or off')
 async def insults(ctx,status):
     global insult_status
     if status == "on" and insult_status == False:
         insult_status = True
-        await ctx.send("i can shit on yall now hehe")
+        await ctx.send("i can insult yall now hehe")
     elif status == "off" and insult_status == True:
         insult_status = False
         await ctx.send("ur no fun but i guess")
     else:
         await ctx.send("that's how it already was stupid")
 
-@bot.event
-async def on_message(msg):
-  await bot.process_commands(msg)
-  if insult_status == True:
-      for i in range(0,df.shape[0]):
-          if int(df.iloc[i][0]) == int(msg.author.id):
-              name = str(df.iloc[i][1])
-              insult = getInsult(name)
-              await msg.channel.send(insult)
-          else:
-              raise error
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+    await client.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.listening, name="!ian"))
 
+
+def getAge(name):
+    webAPI = "https://api.agify.io?name=" + name
+    response = requests.get(webAPI)
+    json_data = json.loads(response.text)
+    age = json_data["age"]
+    return age
+
+@client.event
+async def on_message(msg):
+    await client.process_commands(msg)
+    if insult_status == True:
+        for i in range(0,df.shape[0]):
+            if int(df.iloc[i][0]) == int(msg.author.id):
+                name = str(df.iloc[i][1])
+                insult = getInsult(name)
+                await msg.channel.send(insult)
+            else:
+                raise error
+    elif "POGGERS but text" == msg.content:
+        await msg.channel.send("""
+    ⠄⠄⠄⠄⠄⠄⣀⣀⣀⣤⣶⣿⣿⣶⣶⣶⣤⣄⣠⣴⣶⣿⣿⣿⣿⣶⣦⣄⠄⠄
+    ⠄⠄⣠⣴⣾⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦
+    ⢠⠾⣋⣭⣄⡀⠄⠄⠈⠙⠻⣿⣿⡿⠛⠋⠉⠉⠉⠙⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⡎⣾⡟⢻⣿⣷⠄⠄⠄⠄⠄⡼⣡⣾⣿⣿⣦⠄⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⣿⣿
+    ⡇⢿⣷⣾⣿⠟⠄⠄⠄⠄⢰⠁⣿⣇⣸⣿⣿⠄⠄⠄⠄⠄⠄⠄⣠⣼⣿⣿⣿⣿
+    ⢸⣦⣭⣭⣄⣤⣤⣤⣴⣶⣿⣧⡘⠻⠛⠛⠁⠄⠄⠄⠄⣀⣴⣿⣿⣿⣿⣿⣿⣿
+    ⠄⢉⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣦⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⢰⡿⠛⠛⠛⠛⠻⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⠸⡇⠄⠄⢀⣀⣀⠄⠄⠄⠄⠄⠉⠉⠛⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⠄⠈⣆⠄⠄⢿⣿⣿⣿⣷⣶⣶⣤⣤⣀⣀⡀⠄⠄⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⠄⠄⣿⡀⠄⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠂⠄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⠄⠄⣿⡇⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⠄⠄⣿⡇⠄⠠⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⠄⠄⣿⠁⠄⠐⠛⠛⠛⠛⠉⠉⠉⠉⠄⠄⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿
+    ⠄⠄⠻⣦⣀⣀⣀⣀⣀⣀⣤⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠄
+    """)
+    elif "POGGERS" == msg.content:
+        await msg.channel.send(
+            "https://tenor.com/view/poggers-pepe-gif-12187647")
+    elif "OMEGALUL" == msg.content:
+        await msg.channel.send(
+            "https://tenor.com/view/omegalul-lul-lulw-twitch-emote-gif-13523263"
+        )
+    elif "KEKW" == msg.content:
+        await msg.channel.send(
+            "https://tenor.com/view/kekw-twitch-twitchtv-twitch-emote-kek-gif-19085736"
+        )
+    elif "KEKW but text" == msg.content:
+        await msg.channel.send("""
+    ⢰⣶⠶⢶⣶⣶⡶⢶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⡶⠶⢶⣶⣶⣶⣶
+    ⠘⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⠿⠄⠄⠄⠈⠉⠄⠄⣹⣶⣿⣿⣿⣿⢿
+    ⠄⠤⣾⣿⣿⣿⣿⣷⣤⡈⠙⠛⣿⣿⣿⣧⣀⠠⣤⣤⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣶
+    ⢠⠄⠄⣀⣀⣀⣭⣿⣿⣿⣶⣿⣿⣿⣿⣿⣿⣤⣿⣿⠉⠉⠉⢉⣉⡉⠉⠉⠙⠛⠛
+    ⢸⣿⡀⠄⠈⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣿⣷⣾⣿
+    ⢸⣿⣿⣿⣿⣿⣿⣿⣿⠛⢩⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⢸⣿⣿⣿⣿⣿⡿⣿⣿⣴⣿⣿⣿⣿⣄⣠⣴⣿⣷⣭⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⠸⠿⣿⣿⣿⠋⣴⡟⠋⠈⠻⠿⠿⠛⠛⠛⠛⠛⠃⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⢸⣿⣿⣿⡁⠈⠉⠄⠄⠄⠄⠄⣤⡄⠄⠄⠄⠄⠄⠈⠄⠈⠻⠿⠛⢿⣿⣿⣿⣿⣿
+    ⢸⣿⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⣠⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣀⣿⣿⣿⣿
+    ⢸⣿⣿⣿⡀⠄⠄⠄⠄⠄⠄⠄⠉⠉⠁⠄⠄⠄⠄⠐⠒⠒⠄⠄⠄⠄⠉⢸⣿⣿⣿
+    ⢸⣿⣿⣿⢿⣦⣄⣠⣄⠛⠟⠃⣀⣀⡀⠄⠄⣀⣀⣀⣀⣀⣀⡀⢀⣰⣦⣼⣿⣿⡿
+    ⢸⣿⣿⣿⣿⣿⣿⣻⣿⠄⢰⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢛⣥⣾⣟⣿⣿⣿⣿⣿
+    ⢸⣿⣿⣿⣿⣿⣿⣿⣿⡆⠈⠿⠿⠿⠿⠿⠿⠿⠿⠿⣧⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    """)
+    elif "COCONUT MALL" == msg.content:
+        await msg.channel.send("https://www.youtube.com/watch?v=MoB8QTIvv8E")
+        
+@bot.command(name='ian', help='that\'s me')
+async def ian():
+    await msg.channel.send("what's up lol")
+
+@bot.command(name='ianMagic8Ball', help='test your luck')
+async def ianMagic8Ball():
+    await msg.channel.send(random.choice(ballList))
+
+@bot.command(name='ianInspire', help='feeling down? let me help you')
+async def ianInspire():
+    quote = getQuote()
+    await msg.channel.send(quote)
+
+@bot.command(name='ianJoke', help='feeling sad? let me cheer you up')
+async def ianJoke():
+    joke = getJoke()
+    await msg.channel.send(joke.split("-")[0])
+    time.sleep(2)
+    await msg.channel.send(joke.split("-")[1])
+
+@bot.command(name='ianNameAge', help='i\'ll guess how old you are based on your name')
+async def ianNameAge():
+    ms = msg.content
+    name = ms[12:]
+    print(name)
+    age = getAge(name)
+    await msg.channel.send(age)
+  
+@bot.command(name='ianCat', help='i\'ll send a random cat to lighten the mood')
+async def ianCat():
+    cat = randomCat()
+    await msg.channel.send(cat)
+
+@bot.command(name='ianEncrypt', help='create a caesar cipher as follows: !ianEncrypt index message')
+async def ianEncrypt():
+      ms = msg.content
+      length = len("!ianEncrypt")
+      inputMessage = ms[length:]
+      numRoll = findDigit(inputMessage)
+      inputString = findStr(inputMessage)
+      encryptAnswer = encrypt(numRoll, inputString)
+      await msg.channel.send(encryptAnswer)
+
+@bot.command(name='ianDecrypt', help='decode a caesar cipher as follows: !ianDecrypt index message')
+async def ianDecrypt():
+      ms = msg.content
+      length = len("!ianEncrypt")
+      inputMessage = ms[length:]
+      numRoll = findDigit(inputMessage)
+      inputString = findStr(inputMessage)
+      decryptAnswer = decrypt(numRoll, inputString)
+      await msg.channel.send(decryptAnswer)
+      
 if __name__ == "__main__" :
     bot.run(os.getenv('TOKEN'))
